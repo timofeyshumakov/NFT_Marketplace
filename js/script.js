6c60ab5abc66d3e59d7ac73f9a2b3a2d5a2a0e0d
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2023-10-07 21:07:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
-const els = document.getElementsByClassName('cards');
+const cards = document.getElementsByClassName('cards');
 
+function showMessage() {
 
+  
 setTimeout(() => {
-    const card = els[0].getElementsByClassName('card');
-    if (els[0].clientHeight>= card[0].clientHeight*2){
+    for(var i=0; i<cards.length; i++) {
+    const card = cards[i].getElementsByClassName('card');
+
+    if (cards[i].clientHeight>= card[0].clientHeight*2){
     if (card.length=3){
-  // вставим на страницу новый <li>
-  //document.querySelector('ul').insertAdjacentHTML('beforeend', '<li>Three</li>');
-// выведем количество <li> в консоль
-  console.log(`Количество <li>: ${els.length}`); // 3
-  
-  
-  for(var i=0; i<card.length; i++) {
-    card[i].style.width='50%';
+        
+
+
+
+        if (window.screen.width >= '1200'){
+
+            for(var ii=0; ii<card.length; ii++) {
+           card[ii].style.width='30%';
+           
+        }}
+else{
+
+
+  for(var ii=0; ii<card.length; ii++) {
+    card[ii].style.width='50%';
+
     //alert(card[i].clientHeight)
 }
-    }
 }
-alert(els[0].clientHeight);
-alert(card[0].clientHeight);
-}, 1000);
+    }
+
+}
+
+
+
+}}, 100);
+}
+
+window.onresize = function( event ) {
+    showMessage();
+};
+    
+
