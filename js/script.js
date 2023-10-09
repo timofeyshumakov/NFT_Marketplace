324b4aee92079b5677d1ead201a24b1e81c33e82
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2023-10-09 23:31:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,44 +1,76 @@
-const cards = document.getElementsByClassName('cards');
+//adaptive
 
-function showMessage() {
+var arr = new Map([
+    ['cards', 'card'],
+    ['clients__container', 'clients__img']
+  ]);
 
-  
+  function showMessage() {
+  arr.forEach(function(adaptiveElement,adaptiveBlock) {
+const items = document.getElementsByClassName(adaptiveBlock);
+
 setTimeout(() => {
-    for(var i=0; i<cards.length; i++) {
-    const card = cards[i].getElementsByClassName('card');
 
-    if (cards[i].clientHeight>= card[0].clientHeight*2){
-    if (card.length=3){
+    for(var i=0; i<items.length; i++) {
+    const item = items[i].getElementsByClassName(adaptiveElement);
+    
+if (item.length==3){
+    var ww=0;
+    console.log(item.length);
+    var children = items[i].childNodes;
+    for (var ii = 0; ii < children.length; ++ii) {
+        ww=ww+parseInt(getComputedStyle(children[ii]).width);
         
-
-
-
-        if (window.screen.width >= '1200'){
-
-            for(var ii=0; ii<card.length; ii++) {
-           card[ii].style.width='30%';
-           
-        }}
-else{
-
-
-  for(var ii=0; ii<card.length; ii++) {
-    card[ii].style.width='50%';
-
-    //alert(card[i].clientHeight)
-}
-}
+      }
+      
+    if (items[i].clientHeight >= item[0].clientHeight*3){
+    if (window.screen.width > ww+parseInt(getComputedStyle(items[i]).gap)*2){
+    for(var ii=0; ii<items.length; ii++){
+        items[ii].style.display= 'grid';
+    items[ii].style.gridTemplateColumns = "1fr 1fr 1fr";
+    }}}
+    else{
+    for(var ii=0; ii<items.length; ii++){
+        items[ii].style.display= 'grid';
+        items[ii].style.gridTemplateColumns = "1fr";
+    //alert(item[i].clientHeight)
+}}
     }
-
+    
+if (item.length==4){
+    var ww=0;
+    var children = items[i].childNodes;
+    for (var ii = 0; ii < children.length; ++ii) {
+        ww=ww+parseInt(getComputedStyle(children[ii].firstChild).width);
+        
+      }
+    
+    if (items[i].clientHeight>= item[0].clientHeight*2){
+    if (parseInt(getComputedStyle(items[i]).width)< ww+parseInt(getComputedStyle(items[i]).gap)*3){
+      console.log(items.length);
+    for(var ii=0; ii<items.length; ii++){
+    items[ii].style.display= 'grid';
+    items[ii].style.gridTemplateColumns = "1fr 1fr";
+    }}
+    else{
+    for(var ii=0; ii<items[i].length; ii++){
+    items[ii].style.display= 'flex';
+}}}}
 }
-
-
-
-}}, 100);
+}, 100);
 }
-
+);
+  }
 window.onresize = function( event ) {
     showMessage();
 };
-    
+document.addEventListener("DOMContentLoaded", () => {
+    showMessage()
+  });
+
+//popup
+
+//burger
+
+//scroll
 
